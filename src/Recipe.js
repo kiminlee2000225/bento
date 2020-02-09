57261e2c30c31be24c44026df8927e02dbbfda06
Shine Kim <shinexkim@gmail.com>
2020-02-08 19:21:44 -0500

    Add drawer

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import {Drawer} from '@blueprintjs/core'; 
 
-export default function Recipe({recipes}) {
+export default function Recipes({recipes}) {
+    const [drawer, setDrawer] = React.useState(null);
+    return (
+        <>
+            <RecipeGrid recipes={recipes} setDrawer={setDrawer}/>
+            <Drawer isOpen={!!drawer} onClose={() => setDrawer(null)}>
+                {drawer ? (
+                    <RecipeDrawer recipe={drawer} />
+                ) : <></>}
+            </Drawer>
+        </>
+    )
+}
+
+function RecipeDrawer({recipe}) {
+    return (
+        <div>
+            {recipe.name} {recipe.directions}
+        </div>
+    )
+}
+
+function RecipeGrid({recipes, setDrawer}) {
     const grid = formatGrid(recipes);
     console.log(grid);
     return (
@@ -9,7 +32,7 @@ export default function Recipe({recipes}) {
                 <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
                     {grid.map((row, i) => 
                         <div style={{display: 'flex', flexDirection: 'row'}} key={i}>
-                            {row.map((recipe, j) => <Tile recipe={recipe} key={j} />)}
+                            {row.map((recipe, j) => <Tile recipe={recipe} key={j} setDrawer={setDrawer} />)}
                         </div>)}
                 </div>
             </div>
@@ -34,10 +57,10 @@ function formatGrid(recipes) {
     return res;
 }
 
-function Tile({recipe}) {
+function Tile({recipe, setDrawer}) {
     return(
-        <div style={{height:'300px', width:'300px', margin: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor:'#ffffff'}}>
-            <div style={{height:'150px', width:'150px',borderRadius: '50%', backgroundImage: `url(${recipe.image})`, backgroundSize: 'cover', backgroundPosition: 'center'}}/>
+        <div style={{height:'300px', width:'300px', margin: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor:'#ffffff'}} onClick={() => setDrawer(recipe)}>
+            <div style={{height:'150px', width:'150px',borderRadius: '50%', backgroundImage: `url(${recipe.image})`, backgroundSize: 'cover', backgroundPosition: 'center', margin: '13px'}}/>
             <div style={{fontWeight: 'bold', fontSize: '20px', margin: '10px', color:'#9b9b9b'}}>{recipe.name}</div>
             <div style={{ fontSize: '20px', margin: '10px', fontSize: '15px', bottom:'0', color:'#848484'}}>Prep: {recipe.times[0]} {minHourPrep(recipe)} | Cook: {recipe.times[1]} {minHourCook(recipe)}</div>
         </div>
